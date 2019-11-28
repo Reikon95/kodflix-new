eaf988758fed4f417dbc1fe55c11c2fd137357a1
Cameron Blackwood <cameronjwblackwood@gmail.com>
2019-11-28 14:06:35 +0000

    installed npm start all

--- src/Backend/App.js
+++ src/Backend/App.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello WAA#orld!'))
 app.get('/rest/shows', (req, res) => res.send(shows()))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
