341210a907202c45877fbfcc6c3f2a1b9c389bc2
Cameron Blackwood <cameronjwblackwood@gmail.com>
2019-11-28 12:51:54 +0000

    backend data structure functioning

--- src/Backend/App.js
+++ src/Backend/App.js
@@ -1,7 +1,9 @@
+const shows = require('./Shows')
 const express = require('express')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/rest/shows', (req, res) => res.send(shows()))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ src/Backend/Shows.js
@@ -0,0 +1,12 @@
+function shows() {
+     return [
+        {id: "gameOfThrones", name: "Game Of Thrones", details: 'Nine noble families wage war against each other in order to gain control over the mythical land of Westeros. Meanwhile, a force is rising after millenniums and threatens the existence of living men.'},
+        {id: "breakingBad", name: "Breaking Bad", details: 'Walter White, a chemistry teacher, discovers that he has cancer and decides to get into the meth-making business to repay his medical debts. His priorities begin to change when he partners with Jesse.'},
+        {id: "blackMirror", name: "Black Mirror", details: 'An anthology series that revolves around a group of people\'s personal lives and how technology manipulates their behaviour.'},
+        {id: "theWire", name: "The Wire", details: 'In the city of Baltimore, Detective James McNulty and his team investigate crimes and try their best to solve the bridge that exists between the drug kingpins and the law enforcement agencies.'},
+        {id: "theWalkingDead", name: "The Walking Dead", details: 'A group of survivors led by police officer Rick Grimes travel in search of a safe and secure home after a zombie apocalypse spreads across the USA.'},
+        {id: "strangerThings", name: "Stranger Things", details: 'When Joyce\'s 12-year-old son, Will, goes missing, she launches a terrifying investigation into his disappearance with local authorities. As they search for answers, they unravel a series of extraordinary mysteries involving secret government experiments, unnerving supernatural forces, and a very unusual little girl.'}
+    ];
+}
+
+module.exports = shows;
