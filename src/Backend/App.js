68e2ebba7571c746b705f54490e01a6ac6de386c
Cameron Blackwood <cameronjwblackwood@gmail.com>
2019-12-09 13:53:24 +0000

    added build

--- src/Backend/App.js
+++ src/Backend/App.js
@@ -2,9 +2,14 @@ const shows = require('./Shows')
 const express = require('express')
 const app = express()
 const port = 3001
+const path = require('path');
+
 
 app.get('/', (req, res) => res.send('Hello WAA#orld!'))
 app.get('/rest/shows', (req, res) => res.send(shows()))
-app.use(express.static(path.join(__dirname, '<path-to-build-folder>'))); //CHANGE THIS
 
+app.use(express.static(path.join(__dirname, '../../build')));
+app.get('*', function(req, res) {
+	res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
