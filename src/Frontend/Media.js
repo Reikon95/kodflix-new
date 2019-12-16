69087ddc624ba79ca333ca589411228f0c83a582
Cameron Blackwood <cameronjwblackwood@gmail.com>
2019-12-16 22:02:14 +0000

    refactored and working

--- src/Frontend/Media.js
+++ src/Frontend/Media.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 export default function Media(props) {
     return (
       <Link to={`/${props.id}`} className="tvshow">
-          <img src={props.logo} alt={props.name}/>
-          <div className="overlay">
+          <img src={require(`./Images/${props.id}.jpg`)} alt='' />
+          <div className="overlay">\\
                 <p>{props.name}</p>
               </div>
       </Link>
