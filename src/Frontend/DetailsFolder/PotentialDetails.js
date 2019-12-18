e80457c86e9bd13035b3c53455649162ce14ab11
Cameron Blackwood <cameronjwblackwood@gmail.com>
2019-12-18 11:55:38 +0000

    details component almost finished

--- src/Frontend/DetailsFolder/PotentialDetails.js
+++ src/Frontend/DetailsFolder/PotentialDetails.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+
 
 export default function Covers() {
     const [detail, setDetails] = useState([]);
+    const [welcomeMessage, setWelcomeMessage] = useState("Check back later, website under construction!");
     useEffect(() => {
         fetch('/rest/shows')
         .then(response => response.json())
@@ -12,7 +15,14 @@ export default function Covers() {
     console.log(detail);
     return (
         <>
-        Hello world
+            <div className="Details">
+                <div className="content">
+                <h1>{cover.name}</h1>
+                <p>{cover.details}</p>
+                <img src={cover.logo} alt={cover.name + ' logo'} />
+            </div>
+      <Link to='/'><p>Take me back!</p></Link>
+      </div>
         </>
     )
 
