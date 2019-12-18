754934c628f206293a152f00a1f2dc08a4d5d409
Cameron Blackwood <cameronjwblackwood@gmail.com>
2019-12-18 13:59:51 +0000

    ready for final integration

--- src/Frontend/DetailsFolder/Details.js
+++ src/Frontend/DetailsFolder/Details.js
@@ -1,38 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, Redirect } from 'react-router-dom';
-import getCovers from '../coversGet';
 import './Details.css'
 
-export default class Details extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      welcomeMessage: 'Check back later, website under construction!',
-      cover: {}
-    };
-  }
-  componentDidMount() {
-    let mediaId = this.props.match.params.details;
-    let cover = getCovers().find(function(cover) 
-      {return cover.id === mediaId;});
-    this.setState({
-      cover: cover
-    });
-    }
-  render() {
-    if (this.state.cover === undefined) {
-      return <Redirect to='/not-found'/>;
-    }
-    else {return (
-      <div className="Details">
-      <div className="content">
-        <h1>{this.state.cover.name}</h1>
-        <p>{this.state.cover.details}</p>
-        <img src={this.state.cover.logo} alt={this.state.cover.name + ' logo'} />
-      </div>
+
+
+export default function Covers() {
+    const [detail, setDetails] = useState([]);
+    const [welcomeMessage, setWelcomeMessage] = useState("Check back later, website under construction!");
+    useEffect(() => {
+        fetch('/rest/shows')
+        .then(response => response.json())
+        .then(json => {
+            setDetails(json)
+        })
+    })
+    console.log(detail);
+    
+    return (
+        <>
+            <div className="Details">
+                <div className="content">
+                {/* <h1>{cover.name}</h1>
+                <p>{cover.details}</p>
+                <img src={cover.logo} alt={cover.name + ' logo'} /> */}
+                <h1>Placeholder</h1>
+            </div>
       <Link to='/'><p>Take me back!</p></Link>
       </div>
-    );}    
-  }
-}
+        </>
+    )
 
+}
